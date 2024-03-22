045a998a6c644b2a3742af965f39a508c62d1e59
yog <yogesh_girase@persistent.com>
2024-03-22 17:36:10 +0530

    Questations

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
+import { questionBank } from './data'
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [questions, setquestions] = useState(questionBank);
+  const [selectQuestion, setSelectQuestion] = useState(0);
+  const [isCheck, setIsCheck] = useState(false)
+  const [score, setScore] = useState(0);
+  const calculateScore = () => {
+    setScore((100 * selectQuestion.length) / questions.length);
+
+  }
+  const handleOnChange = (e) => {
+    const selectedQuestationIndex = questions.findIndex((question) => question.id === +e.target.name);
+    const newSelectedQuestions = [{ ...questions[selectedQuestationIndex], selected: e.target.checked }];
+    setSelectQuestion([...questions, ...newSelectedQuestions])
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className='container'>
+        <h1>Questions</h1>
+        {questions?.map((question, index) => (
+          <article key={question.id} className='question'>
+            <header>
+              <h5><span>{index + 1}. </span>{question.question}</h5>
+            </header>
+            <input name={question.id} value={isCheck} type='radio' checked={isCheck[index]} onChange={handleOnChange} />
+            <label>Yes</label>
+            <input name={question.id} value={isCheck} type='radio' checked={isCheck[index]} onChange={handleOnChange} />
+            <label>No</label>
+            {/* <p></p> */}
+          </article>
+        )
+        )}
+        <h1>Score {score}</h1>
+        <button type='btn' onClick={calculateScore}> get Score</button>
+      </section>
+    </main>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,72 @@
+export const questionBank = [
+    {
+        "id": 1,
+        "question": "delectus aut autem",
+        "selected": false
+
+    },
+    {
+
+        "id": 2,
+        "question": "quis ut nam facilis et officia qui",
+        "selected": false
+
+    },
+    {
+
+        "id": 3,
+        "question": "fugiat veniam minus",
+        "selected": false
+
+    },
+    {
+
+        "id": 4,
+        "question": "et porro tempora",
+        "selected": false
+
+    },
+    {
+
+        "id": 5,
+        "question": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+        "selected": false
+
+    },
+    {
+
+        "id": 6,
+        "question": "qui ullam ratione quibusdam voluptatem quia omnis",
+        "selected": false
+
+    },
+    {
+
+        "id": 7,
+        "question": "illo expedita consequatur quia in",
+        "selected": false
+
+
+    },
+    {
+
+        "id": 8,
+        "question": "quo adipisci enim quam ut ab",
+        "selected": false
+
+    },
+    {
+
+        "id": 9,
+        "question": "molestiae perspiciatis ipsa",
+        "selected": false
+
+    },
+    {
+
+        "id": 10,
+        "question": "illo est ratione doloremque quia maiores aut",
+        "selected": false
+
+    },
+]
